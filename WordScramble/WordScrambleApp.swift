44d2cfb2848a49e33b59d9ef81dfe5bd6024ea1c
Chingiz <Dzhabaev@outlook.com>
2025-07-11 22:19:14 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chingiz on 11.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
